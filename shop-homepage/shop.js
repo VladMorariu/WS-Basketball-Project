5c3e6f5e8d9f4d8e2c07778315b3e349b2ca3baa
VladMorariu <vladmihaimorariu@yahoo.com>
2022-02-19 00:57:04 +0200

    carousel

--- shop-homepage/shop.js
+++ shop-homepage/shop.js
@@ -12,3 +12,51 @@ let logoutBtn = document.getElementById('out-btn');
 logoutBtn.addEventListener("click", function (event) {
     localStorage.setItem("loggedUser", {});
 });
+
+
+/* <div class="carousel-case">
+<div class="discount">
+    -20%
+</div>
+<a href="">
+    <img src="images/echipament2.jpg" alt="logo starters stea" title="Logo Starters" height="350">
+</a>
+<h3>Trening negru pentru copii</h3>
+<p><span>399RON</span> 199RON</p>
+</div> */
+
+function addProduct(product) {
+    let carouselDiv = document.createElement("div");
+    carouselDiv.className = "carousel-case";
+    let discountDiv = document.createElement("div");
+    discountDiv.className = "discount";
+    discountDiv.textContent = "-" + product.discount + "%";
+    let img = document.createElement("img");
+    img.src = "../shop-localStorage/" + product.image;
+    img.height = 350;
+    img.alt = "product image";
+    let title = document.createElement("h3");
+    title.textContent = product.name;
+    let span = document.createElement("span");
+    span.textContent = product.price + " RON";
+    let p = document.createElement("p");
+    p.textContent = (product.price - product.discount / 100 * product.price) + " RON ";
+    carouselDiv.appendChild(discountDiv);
+    carouselDiv.appendChild(img);
+    carouselDiv.appendChild(title);
+    carouselDiv.appendChild(p);
+    p.appendChild(span);
+    let carousel = document.getElementById("carousel");
+    // carousel.appendChild(carouselDiv);
+    carousel.insertBefore(carouselDiv, carousel.lastChild);
+    console.log(carousel.lastChild);
+}
+
+let retrievedObject = JSON.parse(localStorage.getItem("0014322323"));
+addProduct(retrievedObject);
+let retrievedObject2 = JSON.parse(localStorage.getItem("0022324432"));
+addProduct(retrievedObject2);
+let retrievedObject3 = JSON.parse(localStorage.getItem("0032342343"));
+addProduct(retrievedObject3);
+
+
--- shop-localStorage/localStorage.js
+++ shop-localStorage/localStorage.js
@@ -1,21 +1,31 @@
 let products = [{
-    name: "Combinezon", image: "images/echipament1.jpg", secondImage: "images/echipament1.1.jpg", price: 199, brand: "adidas", color: "negru", gender: "baiat", discount: 20
+    code: "0014322323", name: "Combinezon", image: "images/echipament1.jpg",  secondImage: "images/echipament1.1.jpg", new: false, recommended: false, price: 199, brand: "adidas", color: "negru", gender: "baiat", discount: 20
 },
 {
-    name: "Trening", image: "images/echipament2.jpg", price: 249, brand: "adidas", color: "negru", gender: "fata", discount: 20
+    code: "0022324432", name: "Trening", image: "images/echipament2.jpg", new: false, recommended: true, price: 249, brand: "adidas", color: "negru", gender: "fata", discount: 20
 },
 {
-    name: "Trening", image: "images/echipament4.jpg", secondImage: "images/echipament4.4.jpg", price: 399, brand: "nike", color: "rosu", gender: "fata", discount: 10
+    code: "0032342343", name: "Trening", image: "images/echipament4.jpg", secondImage: "images/echipament4.4.jpg", new: true, recommended: false, price: 399, brand: "nike", color: "rosu", gender: "fata", discount: 10
 },
 {
-    name: "Trening", image: "images/echipament5.jpg", secondImage: "images/echipament5.5.jpg", price: 399, brand: "nike", color: "negru", gender: "baiat", discount: 30
+    code: "0042342343", name: "Trening", image: "images/echipament5.jpg", secondImage: "images/echipament5.5.jpg", new: true, recommended: true, price: 399, brand: "nike", color: "negru", gender: "baiat", discount: 30
 },
 {
-    name: "Echipament", image: "images/echipament3.jpg", secondImage: "images/echipament3.3.jpg", price: 399, brand: "reebook", color: "alb", gender: "baiat", discount: 25
+    code: "00452342343", name: "Echipament", image: "images/echipament3.jpg", secondImage: "images/echipament3.3.jpg", new: true, recommended: false, price: 399, brand: "reebook", color: "alb", gender: "baiat", discount: 25
 },
 {
-    name: "Rucsac", image: "images/accesorii1.jpg", secondImage: "images/accesorii1.1.jpg", price: 399, brand: "nike", color: "negru", gender: "baiat", discount: 8
+    code: "00462342343", name: "Rucsac", image: "images/accesorii1.jpg", secondImage: "images/accesorii1.1.jpg", new: false, recommended: false, price: 399, brand: "nike", color: "negru", gender: "baiat", discount: 8
 },
 {
-    name: "Rucsac", image: "images/accesorii2.jpg", secondImage: "images/accesorii2.2.jpg", price: 399, brand: "nike", color: "rosu", gender: "fata", discount: 0
-}]
+    code: "00472342343", name: "Rucsac", image: "images/accesorii2.jpg", secondImage: "images/accesorii2.2.jpg", new: true, recommended: true, price: 399, brand: "nike", color: "rosu", gender: "fata", discount: 0
+}];
+
+// Code used to add products to localstorage
+// for (let product of products) {
+//     localStorage.setItem(product.code, JSON.stringify(product));
+// }
+
+for (let product of products) {
+    let retrievedObject = JSON.parse(localStorage.getItem(product.code));
+    console.log(JSON.parse(localStorage.getItem(retrievedObject.code)));
+}
