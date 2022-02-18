f766e03ea80ad99587f4b4711376be75889c0a3e
VladMorariu <vladmihaimorariu@yahoo.com>
2022-02-18 22:20:38 +0200

    signin js si mici modificari in html si css

---
+++ shop-signin/signin.js
@@ -0,0 +1,13 @@
+const signinForm = document.getElementById('signinForm');
+signinForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let retrievedObject = JSON.parse(localStorage.getItem(email));
+    if (password == retrievedObject.password){
+        location.href="../shop-homepage/shop.html";
+    } else {
+        let message = document.getElementById('message');
+        message.style.display = 'block';
+    }
+});
