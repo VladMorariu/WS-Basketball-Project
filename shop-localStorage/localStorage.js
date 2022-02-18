0694f71dcffdb906d809749c88431400d5d62eed
VladMorariu <vladmihaimorariu@yahoo.com>
2022-02-18 23:42:39 +0200

    am creat un ls folder

---
+++ shop-localStorage/localStorage.js
@@ -0,0 +1,21 @@
+let products = [{
+    name: "Combinezon", image: "images/echipament1.jpg", secondImage: "images/echipament1.1.jpg", price: 199, brand: "adidas", color: "negru", gender: "baiat", discount: 20
+},
+{
+    name: "Trening", image: "images/echipament2.jpg", price: 249, brand: "adidas", color: "negru", gender: "fata", discount: 20
+},
+{
+    name: "Trening", image: "images/echipament4.jpg", secondImage: "images/echipament4.4.jpg", price: 399, brand: "nike", color: "rosu", gender: "fata", discount: 10
+},
+{
+    name: "Trening", image: "images/echipament5.jpg", secondImage: "images/echipament5.5.jpg", price: 399, brand: "nike", color: "negru", gender: "baiat", discount: 30
+},
+{
+    name: "Echipament", image: "images/echipament3.jpg", secondImage: "images/echipament3.3.jpg", price: 399, brand: "reebook", color: "alb", gender: "baiat", discount: 25
+},
+{
+    name: "Rucsac", image: "images/accesorii1.jpg", secondImage: "images/accesorii1.1.jpg", price: 399, brand: "nike", color: "negru", gender: "baiat", discount: 8
+},
+{
+    name: "Rucsac", image: "images/accesorii2.jpg", secondImage: "images/accesorii2.2.jpg", price: 399, brand: "nike", color: "rosu", gender: "fata", discount: 0
+}]
