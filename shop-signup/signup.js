90a74531e632829951bbfb06548d20d581039a0f
VladMorariu <vladmihaimorariu@yahoo.com>
2022-02-18 21:22:58 +0200

    signup js

---
+++ shop-signup/signup.js
@@ -0,0 +1,23 @@
+const signupForm = document.getElementById('signupForm');
+signupForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let user = { };
+    user.firstName=document.getElementById('firstname').value;
+    user.lastname=document.getElementById('lastname').value;
+    user.phone=document.getElementById('phone').value;
+    user.password=document.getElementById('password').value;
+    user.news=document.getElementById('news').checked;
+    user.gdpr=document.getElementById('gdpr').checked;
+    
+    let email = document.getElementById('email').value;
+    localStorage.setItem(email, JSON.stringify(user));
+    location.href="../shop-signin/signin.html"
+
+// Retrieve the object from storage
+var retrievedObject = localStorage.getItem(email);
+
+console.log('retrievedObject: ', JSON.parse(retrievedObject));
+});
+
+
+
